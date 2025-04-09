0e69fb5993f54959d46b7c6b123ba926a66303bc
BAGATHE <lolemadaly@gmail.com>
2025-04-09 21:21:24 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button className="text-white hover:bg-green-800">Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
